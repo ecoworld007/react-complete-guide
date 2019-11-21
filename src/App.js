8733aca94ee2d859ce96c1a675829f6b66159ce6
Himanshu Negi <hnegi@sapient.com>
2019-11-21 15:16:47 +0530

    using state property of the react class component

--- src/App.js
+++ src/App.js
@@ -2,13 +2,20 @@ import React, { Component } from 'react';
 import './App.css';
 import Person from './Person/Person';
 class App extends Component {
+  state = {
+    persons : [
+      {name: 'Himanshu', age: 26},
+      {name: 'Vinay', age: 27},
+      {name: 'Rohit', age: 28}
+    ]
+  }
   render() {
     return (
       <div className="App">
           <h1>Welcome to React</h1>
-          <Person name='Himanshu' age='26'>My Hobbies is cycling</Person>
-          <Person name='Vinay' age='27'/>
-          <Person name='Rohit' age='28'/>
+          <Person name={this.state.persons[0].name} age={this.state.persons[0].age}>My Hobbies is cycling</Person>
+          <Person name={this.state.persons[1].name} age={this.state.persons[1].age}/>
+          <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
       </div>
     );
     // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Welcome to React'));
