26ec8dc522d8d313d7cbe1add3c5d49f77acb9a7
Himanshu Negi <hnegi@sapient.com>
2019-11-21 15:30:52 +0530

    first event handler usage

--- src/App.js
+++ src/App.js
@@ -9,10 +9,14 @@ class App extends Component {
       {name: 'Rohit', age: 28}
     ]
   }
+  switchNameHandler = () => {
+    console.log('was clicked!');
+  }
   render() {
     return (
       <div className="App">
           <h1>Welcome to React</h1>
+          <button onClick={this.switchNameHandler}>Switch Name</button>
           <Person name={this.state.persons[0].name} age={this.state.persons[0].age}>My Hobbies is cycling</Person>
           <Person name={this.state.persons[1].name} age={this.state.persons[1].age}/>
           <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
