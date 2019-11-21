861450d240784e348b7df6ff568dd83762a59e29
Himanshu Negi <hnegi@sapient.com>
2019-11-21 16:09:43 +0530

    converting the class component to functional component and changing the state using hook

--- src/App.js
+++ src/App.js
@@ -1,37 +1,35 @@
-import React, { Component } from 'react';
+import React, {useState} from 'react';
 import './App.css';
 import Person from './Person/Person';
-class App extends Component {
-  state = {
+const App = () => {
+  let [personsState, setPersonsState] = useState({
     persons : [
       {name: 'Himanshu', age: 26},
       {name: 'Vinay', age: 27},
       {name: 'Rohit', age: 28}
     ]
-  }
-  switchNameHandler = () => {
+  });
+  const switchNameHandler = () => {
     console.log('was clicked!');
     // Don't Use this to change state this.state.persons = [{name: 'Himanshu', age: 26}, {name: 'Vinay', age: 27}, {name: 'Rohit', age: 28}]
-    this.setState({
+    setPersonsState({
       persons: [
         {name: 'Himanshu Negi', age: 26},
         {name: 'Vinay', age: 27},
         {name: 'Rohit', age: 29}
       ]
-    })
+    });
   }
-  render() {
     return (
       <div className="App">
           <h1>Welcome to React</h1>
-          <button onClick={this.switchNameHandler}>Switch Name</button>
-          <Person name={this.state.persons[0].name} age={this.state.persons[0].age}>My Hobbies is cycling</Person>
-          <Person name={this.state.persons[1].name} age={this.state.persons[1].age}/>
-          <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
+          <button onClick={switchNameHandler}>Switch Name</button>
+          <Person name={personsState.persons[0].name} age={personsState.persons[0].age}>My Hobbies is cycling</Person>
+          <Person name={personsState.persons[1].name} age={personsState.persons[1].age}/>
+          <Person name={personsState.persons[2].name} age={personsState.persons[2].age}/>
       </div>
     );
     // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Welcome to React'));
-  }
 }
 
 export default App;
