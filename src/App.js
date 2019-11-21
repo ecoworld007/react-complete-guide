dda263a1e4147a0d9546f6c86b2675a3227391e1
Himanshu Negi <hnegi@sapient.com>
2019-11-21 16:25:16 +0530

    using the class component again for root componet

--- src/App.js
+++ src/App.js
@@ -1,35 +1,37 @@
-import React, {useState} from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import Person from './Person/Person';
-const App = () => {
-  let [personsState, setPersonsState] = useState({
+class App extends Component {
+  state = {
     persons : [
       {name: 'Himanshu', age: 26},
       {name: 'Vinay', age: 27},
       {name: 'Rohit', age: 28}
     ]
-  });
-  const switchNameHandler = () => {
+  }
+  switchNameHandler = () => {
     console.log('was clicked!');
     // Don't Use this to change state this.state.persons = [{name: 'Himanshu', age: 26}, {name: 'Vinay', age: 27}, {name: 'Rohit', age: 28}]
-    setPersonsState({
+    this.setState({
       persons: [
         {name: 'Himanshu Negi', age: 26},
         {name: 'Vinay', age: 27},
         {name: 'Rohit', age: 29}
       ]
-    });
+    })
   }
+  render() {
     return (
       <div className="App">
           <h1>Welcome to React</h1>
-          <button onClick={switchNameHandler}>Switch Name</button>
-          <Person name={personsState.persons[0].name} age={personsState.persons[0].age}>My Hobbies is cycling</Person>
-          <Person name={personsState.persons[1].name} age={personsState.persons[1].age}/>
-          <Person name={personsState.persons[2].name} age={personsState.persons[2].age}/>
+          <button onClick={this.switchNameHandler}>Switch Name</button>
+          <Person name={this.state.persons[0].name} age={this.state.persons[0].age}>My Hobbies is cycling</Person>
+          <Person name={this.state.persons[1].name} age={this.state.persons[1].age}/>
+          <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
       </div>
     );
     // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Welcome to React'));
+  }
 }
 
 export default App;
