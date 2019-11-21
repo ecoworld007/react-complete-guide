c3e15970817b4479d63edf71e46b557633fe4eb1
Himanshu Negi <hnegi@sapient.com>
2019-11-21 14:14:09 +0530

    first component called person in use

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import './App.css';
-
+import Person from './Person/Person';
 class App extends Component {
   render() {
     return (
       <div className="App">
           <h1>Welcome to React</h1>
+          <Person/>
       </div>
     );
     // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Welcome to React'));
