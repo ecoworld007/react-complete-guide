10871eb25967917d15316dff97ec7e2a74b9caea
Himanshu Negi <hnegi@sapient.com>
2019-11-22 12:18:27 +0530

    adding styling to person component

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,8 +1,8 @@
 import React from 'react';
-
+import './Person.css';
 const person = (props) => {
   return (
-    <div>
+    <div className='Person'>
       <p onClick={props.click} >I am {props.name} and my age is {props.age} year</p>
       <p>{props.children}</p>
       <input type='text' onChange={props.nameChanged} value={props.name}/>
