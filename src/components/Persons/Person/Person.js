5eab14cb4da982f929598fd7fd799db50af090c9
Himanshu Negi <hnegi@sapient.com>
2019-11-27 14:24:21 +0530

    higher order component(hoc)

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
-import classes from './Person.css'
+import classes from './Person.css';
+import Aux from '../../../hoc/Aux';
 class Person extends Component{
   // static getDerivedStateFromProps(props, state){
   //   console.log('[Person.js] getDerivedStateFromProps');
@@ -20,11 +21,11 @@ class Person extends Component{
   render() {
     console.log('[Person.js] rendering...');
     return (
-      <div className={classes.Person}>
+      <Aux className={classes.Person}>
         <p onClick={this.props.click} >I am {this.props.name} and my age is {this.props.age} year</p>
         <p>{this.props.children}</p>
         <input type='text' onChange={this.props.nameChanged} value={this.props.name}/>
-      </div>
+      </Aux>
     )
   }
 }
