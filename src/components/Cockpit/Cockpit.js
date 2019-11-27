6a247bab4267f28ac08f2f92cf2aec8446350aca
Himanshu Negi <hnegi@sapient.com>
2019-11-27 16:41:43 +0530

    using ref in functional component

--- src/components/Cockpit/Cockpit.js
+++ src/components/Cockpit/Cockpit.js
@@ -1,11 +1,13 @@
-import React, {useEffect} from 'react';
+import React, {useEffect, useRef} from 'react';
 import classes from './Cockpit.css';
 const cockpit = (props) => {
+  const toggleBtnRef = useRef(null);
   useEffect(() => {
     console.log('[Cockpit.js] useEffect');
-    setTimeout(() => {
-      alert('saved data to cloud');
-    }, 1000);
+    // setTimeout(() => {
+    //   alert('saved data to cloud');
+    // }, 1000);
+    toggleBtnRef.current.click();
     return () => {
         console.log('[Cockpit.js] clean work in useeffect');
       }
@@ -31,7 +33,7 @@ const cockpit = (props) => {
     <div className={classes.Cockpit}>
       <h1>Welcome to React</h1>
       <p className={assignedClasses.join(' ')}>This is working</p>
-      <button className={btnClass} onClick={props.toggle}>Toggle Persons</button>
+      <button ref={toggleBtnRef} className={btnClass} onClick={props.toggle}>Toggle Persons</button>
     </div>
   );
 }
